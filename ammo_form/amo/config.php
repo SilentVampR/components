4681b3f69362b9c9e17589356a540bf6f5ac7187
SilentVampR <johnny.1983@yandex.ru>
2023-02-14 23:59:31 +0700

    add php files for integration

---
+++ ammo_form/amo/config.php
@@ -0,0 +1,7 @@
+<?php
+$subdomain     = ''; // поддомен AmoCRM
+$client_secret = ''; // Секретный ключ
+$client_id     = ''; // ID интеграции
+$code          = ''; // Код авторизации
+$token_file    = 'tokens.txt';
+$redirect_uri  = "https://" . $_SERVER['HTTP_HOST'] . '/amo/amo.php';
